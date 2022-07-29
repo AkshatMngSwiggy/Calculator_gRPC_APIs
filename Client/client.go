1976872be5105b474292fb9f3b1c56fbe9662916
AkshatMngSwiggy <akshat.mangal@swiggy.in>
2022-07-29 10:47:10 +0530

    second commit

--- Client/client.go
+++ Client/client.go
@@ -8,6 +8,8 @@ import (
 	"time"
 
 	"calculator/calcpb"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -15,12 +17,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not connect: %v", err)
 	}
-	defer cc.close()
+	defer cc.Close()
 
 	c := calcpb.NewCalculatorServiceClient(cc)
 
 	// Sum function
-	// Sum(c)
+	Sum(c)
 
 	// Prime Number
 	// PrimeNumber(c)
@@ -35,7 +37,7 @@ func main() {
 func Sum(c calcpb.CalculatorServiceClient) {
 	fmt.Println("Starting sending two numbers for Sum")
 	req := calcpb.CalculatorRequest{
-		Sum: &calcpb.CalculatorRequest{
+		Sum: &calcpb.Calculator{
 			a: 4,
 			b: 5,
 		},
@@ -53,8 +55,8 @@ func PrimeNumber(c calcpb.CalculatorServiceClient) {
 	fmt.Println("Ready to receive prime numbers...")
 
 	req := calcpb.PrimeNumberRequest{
-		Number: &calcpb.Number{
-			p: 20,
+		P: &calcpb.Number{
+			P: 20,
 		},
 	}
 
@@ -82,139 +84,140 @@ func ComputeAverage(c calcpb.CalculatorServiceClient) {
 		log.Fatalf("error occured while performing client-side streaming: %v", err)
 	}
 
-	requests := []*calcpb.ComputeAverageRequest{}{
+	requests := []*calcpb.ComputeAverageRequest{
 		&calcpb.ComputeAverageRequest{
-			Number: &calcpb.Number{
+			P: &calcpb.Number{
 				P: 4,
 			},
 		},
 		&calcpb.ComputeAverageRequest{
-			Number: &calcpb.Number{
+			P: &calcpb.Number{
 				P: 5,
 			},
 		},
 		&calcpb.ComputeAverageRequest{
-			Number: &calcpb.Number{
+			P: &calcpb.Number{
 				P: 9,
 			},
 		},
 		&calcpb.ComputeAverageRequest{
-			Number: &calcpb.Number{
+			P: &calcpb.Number{
 				P: 13,
 			},
 		},
 		&calcpb.ComputeAverageRequest{
-			Number: &calcpb.Number{
+			P: &calcpb.Number{
 				P: 25,
 			},
 		},
 		&calcpb.ComputeAverageRequest{
-			Number: &calcpb.Number{
+			P: &calcpb.Number{
 				P: 4,
 			},
 		},
 	}
 
-	for _, req := range requests{
+	for _, req := range requests {
 		fmt.Println("\nSending Request...", req)
 		stream.Send(req)
-		time.Sleen(1*time.Second)
+		time.Sleep(1 * time.Second)
 	}
 
 	resp, err := stream.CloseAndRecv()
-	if err!=nil{
+	if err != nil {
 		log.Fatalf("Error while receiving response from server: %v", err)
 	}
 	fmt.Println("\n Computed Average: ", resp.GetResult())
- }
+}
 
- func FindMaxNumber(c calcpb.CalculatorServiceClient){
+func FindMaxNumber(c calcpb.CalculatorServiceClient) {
 	fmt.Println("Finding the Max number over the incoming stream of numbers...")
 
-	requests := []*calcpb.FindMaxNumberRequest{
-		&calcpb.FindMaxNumberRequest{
-			Number: &calcpb.Number{
+	requests := []*calcpb.FindmaxNumberRequest{
+		&calcpb.FindmaxNumberRequest{
+			P: &calcpb.Number{
 				P: 1,
 			},
 		},
-		&calcpb.FindMaxNumberRequest{
-			Number: &calcpb.Number{
+		&calcpb.FindmaxNumberRequest{
+			P: &calcpb.Number{
 				P: 3,
 			},
 		},
-		&calcpb.FindMaxNumberRequest{
-			Number: &calcpb.Number{
+		&calcpb.FindmaxNumberRequest{
+			P: &calcpb.Number{
 				P: 7,
 			},
 		},
-		&calcpb.FindMaxNumberRequest{
-			Number: &calcpb.Number{
+		&calcpb.FindmaxNumberRequest{
+			P: &calcpb.Number{
 				P: 9,
 			},
 		},
-		&calcpb.FindMaxNumberRequest{
-			Number: &calcpb.Number{
+		&calcpb.FindmaxNumberRequest{
+			P: &calcpb.Number{
 				P: 2,
 			},
 		},
-		&calcpb.FindMaxNumberRequest{
-			Number: &calcpb.Number{
+		&calcpb.FindmaxNumberRequest{
+			P: &calcpb.Number{
 				P: 5,
 			},
 		},
-		&calcpb.FindMaxNumberRequest{
-			Number: &calcpb.Number{
+		&calcpb.FindmaxNumberRequest{
+			P: &calcpb.Number{
 				P: 22,
 			},
 		},
-		&calcpb.FindMaxNumberRequest{
-			Number: &calcpb.Number{
+		&calcpb.FindmaxNumberRequest{
+			P: &calcpb.Number{
 				P: 15,
 			},
 		},
-		&calcpb.FindMaxNumberRequest{
-			Number: &calcpb.Number{
+		&calcpb.FindmaxNumberRequest{
+			P: &calcpb.Number{
 				P: 21,
 			},
 		},
-		&calcpb.FindMaxNumberRequest{
-			Number: &calcpb.Number{
+		&calcpb.FindmaxNumberRequest{
+			P: &calcpb.Number{
 				P: 19,
 			},
 		},
 	}
 
 	stream, err := c.FindMaxNumber(context.Background())
-	if err!=nil{
+	if err != nil {
 		log.Fatalf("error occured while performing client side streaming: %v", err)
 	}
 
 	waitchan := make(chan struct{})
 
-	go func(requests []*calcpb.FindMaxNumberRequest){
-		for _, req := range requests{
+	go func(requests []*calcpb.FindmaxNumberRequest) {
+		for _, req := range requests {
 			fmt.Println("\n Sending Request... : ", req.Calculator)
 			err := stream.Send(req)
-			if err!=nil{
+			if err != nil {
 				log.Fatalf("error while sending request to FindMaxNUmber service: %v", err)
 			}
-			time.Sleep(1000*time.Millisecond)
-		}(requests)
+			time.Sleep(1000 * time.Millisecond)
+		}
+		stream.CloseSend()
+	}(requests)
 
-		go func(){
-			for {
-				resp, err := stream.Recv()
-				if err==io.EOF{
-					close(waitchan)
-					return
-				}
-				if err !=nil{
-					log.Fatalf("error while receiving response from server: %v", err)
-				}
-				fmt.Printf("\n Max Element till now in the Stream: %v", resp.GetResult())
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				close(waitchan)
+				return
 			}
-		}()
+			if err != nil {
+				log.Fatalf("error while receiving response from server: %v", err)
+			}
+			fmt.Printf("\n Max Element till now in the Stream: %v", resp.GetResult())
+		}
+	}()
 
-		<-waitchan
-	}
- }
+	<-waitchan
+}
--- Server/server.go
+++ Server/server.go
@@ -6,24 +6,27 @@ import (
 	"io"
 	"log"
 	"math"
+	"net"
 	"time"
 
 	"calculator/calcpb"
+
+	"google.golang.org/grpc"
 )
 
 type server struct {
-	calcpb.UnimplementedGreetServiceServer
+	calcpb.UnimplementedCalculatorServiceServer
 }
 
 func (*server) Sum(ctx context.Context, req *calcpb.SumRequest) (resp *calcpb.SumResponse, err error) {
 	fmt.Println("Implementing Sum function of Calculator")
 
-	a := req.GetSum().GetA()
-	b := req.GetSUm().GetB()
+	a := req.GetS().GetA()
+	b := req.GetS().GetB()
 
 	result := a + b
 
-	res := fmt.Sprintf("Sum of %v and %v equal to %v", a, b, result)
+	res := result
 
 	resp = &calcpb.SumResponse{
 		Result: res,
@@ -45,15 +48,15 @@ func isprime(n int) bool {
 	return true
 }
 
-func (*server) PrimeNumber(ctx context.Context, req *calcpb.PrimeNumberRequest, resp *calcpb.PrimeNumberResponse) error {
+func (*server) PrimeNumber(req *calcpb.PrimeNumberRequest, resp calcpb.CalculatorService_PrimeNumberServer) error {
 	fmt.Println("Implementing Prime Numbers Operation")
 
-	p := req.GetNumber().GetP()
+	p := req.GetP().GetP()
 
-	for i := 0; i < p; i++ {
+	for i := 0; int64(i) < p; i++ {
 		isp := isprime(i)
 		if isp {
-			result := fmt.Sprintf("Prime Number between 0 and %v: %v", p, i)
+			result := int64(i)
 			res := calcpb.PrimeNumberResponse{
 				Result: result,
 			}
@@ -67,14 +70,14 @@ func (*server) PrimeNumber(ctx context.Context, req *calcpb.PrimeNumberRequest, 
 func (*server) ComputeAverage(stream calcpb.CalculatorService_ComputeAverageServer) error {
 	fmt.Println("Implementing Computer Average Function")
 	result := 0.0
-	count := 0.0
-	res := 0.0
+	count := int64(0)
+	res := int64(0)
 
 	for {
 		msg, err := stream.Recv()
 		if err == io.EOF {
-			result = res / count
-			return stream.SendAndClose(&calcpb.ComputeAverage{
+			result = float64(res / count)
+			return stream.SendAndClose(&calcpb.ComputeAverageResponse{
 				Result: result,
 			})
 		}
@@ -84,7 +87,7 @@ func (*server) ComputeAverage(stream calcpb.CalculatorService_ComputeAverageServ
 		}
 
 		count += 1
-		res += msg.GetNumber().GetP()
+		res += msg.GetP().GetP()
 	}
 
 }
@@ -101,19 +104,18 @@ func (*server) FindMaxNumber(stream calcpb.CalculatorService_FindMaxNumberServer
 			log.Fatal("error while receiving data from FindMaxNUmber client: %v", err)
 			return err
 		}
-		max := 0
-		num := req.GetNumber().GetP()
+		max := int64(0)
+		num := req.GetP().GetP()
 
 		if max < num {
 			result := max
 			sendErr := stream.Send(&calcpb.FindmaxNumberResponse{
 				Result: result,
 			})
-		}
-
-		if sendErr != nil {
-			log.Fatalf("error while sending response to FindMacNumber Client: %v", err)
-			return err
+			if sendErr != nil {
+				log.Fatalf("error while sending response to FindMacNumber Client: %v", err)
+				return err
+			}
 		}
 	}
 }
@@ -121,13 +123,13 @@ func (*server) FindMaxNumber(stream calcpb.CalculatorService_FindMaxNumberServer
 func main() {
 	fmt.Println("Server is Listening...")
 
-	listen, err = net.listen("tcp", "1.0.1.0:1010")
+	listen, err := net.Listen("tcp", "0.0.0.0:1010")
 	if err != nil {
 		log.Fatal("Failed to Listen: %v", err)
 	}
 
 	s := grpc.NewServer()
-	calcpb.RegisterGreetServiceServer(s, &server{})
+	calcpb.RegisterCalculatorServiceServer(s, &server{})
 
 	if err = s.Serve(listen); err != nil {
 		log.Fatal("Failed to Serve: %v", err)
